4fd38da8310ee4b5f2e93f8590595dfa37bf19b7
Carlos Monteiro <carlosem4il@gmail.com>
2025-05-28 00:09:06 -0300

    adding ring api / creating a basic endpoint

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [ring/ring-core "2.0.0-alpha1"]
+                 [ring/ring-jetty-adapter "2.0.0-alpha1"]
+                 [compojure "1.7.1"]
+                 ; A concise routing library for Ring
+                 ]
   :main ^:skip-aot clojure-simple-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- src/clojure_simple_api/core.clj
+++ src/clojure_simple_api/core.clj
@@ -1,7 +1,21 @@
 (ns clojure-simple-api.core
+  (:require [ring.adapter.jetty :refer [run-jetty]])
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route])
+  ;(:use ring.adapter.jetty)
   (:gen-class))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello World"})
+
+(defroutes
+  (GET "/" [] "<h1>Hello World</h1>")
+  (route/not-found "<h1>Page not found</h1>"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Hello, World!")
+  (run-jetty handler {:port 3000}))
